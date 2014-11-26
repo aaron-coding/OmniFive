0029ff82c54b07dab7ea7a1dfcfb45edd254e4b3
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-11-26 14:14:30 -0800

    Refactor gigs index to use composite view

--- app/assets/javascripts/views/gigs/gigs_index.js
+++ app/assets/javascripts/views/gigs/gigs_index.js
@@ -5,20 +5,23 @@ Gigrr.Views.GigsIndex = Backbone.CompositeView.extend({
   initialize: function(){
     this.listenTo(this.collection, "sync", this.render)
     this.$el.addClass("container-fluid")
-    
   },
   
   render: function(){
     var renderedContent = this.template({ gigs: this.collection });
     this.$el.html(renderedContent);
-    var that = this;
-    this.collection.each(function(gig) {
-      var gigView = new Gigrr.Views.GigsIndexItem({ model: gig });
-      that.$(".gigs-index").append(gigView.render().$el);
-    });
-      
+    this.addGigsSubviews();
     return this;
   },
+  
+  addGigsSubviews: function(){
+    //All gigs in this collection are added to the page as their own view
+    var that = this;
+    this.collection.each(function(gig) { 
+      var gigView = new Gigrr.Views.GigsIndexItem({ model: gig });
+      that.addSubview(".gigs-index", gigView);
+    });    
+  }
 
   
   
