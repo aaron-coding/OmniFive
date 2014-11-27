33f33b1540a1b4960c533808707c297975cce40a
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-11-26 17:09:55 -0800

    Put gig extras into table format

--- app/assets/javascripts/views/gig_extras/gig_extras_show.js
+++ app/assets/javascripts/views/gig_extras/gig_extras_show.js
@@ -1,7 +1,7 @@
 Gigrr.Views.GigExtrasShow = Backbone.View.extend({
 
   template: JST['gig_extras/show'],
-  
+  tagName: "<tr>",
   initialize: function(){
     this.listenTo(this.model, "sync", this.render)
   },
