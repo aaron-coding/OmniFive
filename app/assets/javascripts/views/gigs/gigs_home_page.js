8982931e81ddb24ab580b608fc7df527c4ab4033
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-12-05 11:57:01 -0800

    Minor Cleanup

--- app/assets/javascripts/views/gigs/gigs_home_page.js
+++ app/assets/javascripts/views/gigs/gigs_home_page.js
@@ -4,8 +4,13 @@ Gigrr.Views.GigsHomePage = Backbone.CompositeView.extend({
   
   initialize: function(){
     this.listenTo(this.collection, "sync", this.render)
+    $(".guest-login").click(this.guestLogin);
   },
-    
+  
+  guestLogin: function(){
+    alert("guest login! w00t");
+  },
+  
   render: function(){
     var renderedContent = this.template();
     this.$el.html(renderedContent);
